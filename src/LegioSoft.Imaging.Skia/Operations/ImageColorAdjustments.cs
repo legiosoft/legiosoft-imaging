3996b34c47ce61c4de8c45847775b1b6b53624b9
Leonid <tops@topkek.kek>
2026-01-15 22:19:53 +0100

    Update ImageColorAdjustments.cs

--- src/LegioSoft.Imaging.Skia/Operations/ImageColorAdjustments.cs
+++ src/LegioSoft.Imaging.Skia/Operations/ImageColorAdjustments.cs
@@ -137,9 +137,9 @@ internal static class ImageColorAdjustments
 
         paint.ColorFilter = SKColorFilter.CreateColorMatrix(new float[]
         {
-            -1, 0, 0, 0, 255,
-            0, -1, 0, 0, 255,
-            0, 0, -1, 0, 255,
+            -1, 0, 0, 0, 1,
+            0, -1, 0, 0, 1,
+            0, 0, -1, 0, 1,
             0, 0, 0, 1, 0
         });
 
